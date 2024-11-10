103e26b26a3fcf6263dc1972f06d75aad482c5e7
IzuEneh <izueneh21@gmail.com>
2024-11-09 19:43:24 -0500

    add list of mock transactions

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,75 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import {
+	Image,
+	StyleSheet,
+	Platform,
+	View,
+	FlatList,
+	Text,
+	SafeAreaView,
+} from "react-native";
+import { useEffect, useState } from "react";
 
 export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+	const [transactions, setTransactions] = useState<any[]>([]);
+	useEffect(() => {
+		const getTransactions = async () => {
+			const resp = await fetch(
+				"https://api.firstplaidypusbank.plaid.com/fdx/v5/accounts/deposit_01_checking/transactions?startTime=2022-01-30&endTime=2022-05-30",
+				{
+					headers: {
+						Authorization: "Bearer 000000000001",
+					},
+				}
+			);
+
+			if (!resp.ok) {
+				throw new Error(`Failed to fetch transaction: ${resp.status}`);
+			}
+
+			const json = await resp.json();
+			setTransactions(json.transactions);
+			console.log("complete");
+		};
+
+		getTransactions();
+	}, []);
+
+	return (
+		<SafeAreaView>
+			{/* <Text style={{ color: "white" }}>Fetching TRansactions</Text> */}
+			<FlatList
+				data={transactions}
+				renderItem={({ item }) => (
+					<View>
+						<Text style={{ color: "white" }}>
+							{item.depositTransaction.description}
+						</Text>
+						<Text style={{ color: "white" }}>
+							{item.depositTransaction.amount}
+						</Text>
+					</View>
+				)}
+				ItemSeparatorComponent={() => <View style={{ height: 16 }} />}
+			/>
+		</SafeAreaView>
+	);
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+	titleContainer: {
+		flexDirection: "row",
+		alignItems: "center",
+		gap: 8,
+	},
+	stepContainer: {
+		gap: 8,
+		marginBottom: 8,
+	},
+	reactLogo: {
+		height: 178,
+		width: 290,
+		bottom: 0,
+		left: 0,
+		position: "absolute",
+	},
 });
